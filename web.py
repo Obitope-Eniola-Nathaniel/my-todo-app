e5a7fe1d4d47efa10ca08d057902bfa120a3dbe8
Obitope Eniola Nathaniel <obitopeeniola@gamil.com>
2024-02-16 09:29:17 +0100

    Implement "Checkout todo items" feature

--- web.py
+++ web.py
@@ -17,12 +17,17 @@ st.title("Welcome to Nathaniel Todo App")
 st.subheader("This is my todo app.")
 st.write('This app is to increase your productivity')
 
-# A for loop to read the text messages to checkbox
-for todo in todos:
-    st.checkbox(todo)
+# A for loop to read the text messages to checkbox and also remove checkbox when click
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        function.write_todo(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 # Receive input from users
-st.text_input(label=" ", placeholder="Add new todo...",
-              on_change=add_todo, key="new_todo")
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
 
 st.session_state
